a53bbf09655822c4314d46094a720ab60b1a67e1
Oleksandr <bilovka10@gmail.com>
2020-08-14 15:43:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array
 }
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length-1)=array
+  array=array.slice(0, array.length-1)
   return array
 }
 
